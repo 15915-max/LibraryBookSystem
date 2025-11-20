cc7fb9d589ca5d8e7572547b4d5c2193225255a2
15915-max <15915@ait.nsw.edu.au>
2025-11-20 19:18:49 +1100

    modified code

--- Book.h
+++ Book.h
@@ -1,4 +1,4 @@
-#ifndef BOOK_H   // Include guard
+#ifndef BOOK_H
 #define BOOK_H
 
 #include <iostream>
@@ -14,21 +14,51 @@ private:
     string dateAdd;
 
 public:
-    // --- Set book details ---
-    void setBookDetails(string t, string a, string i, bool avail, string date);
+    // Set book details
+    void setBookDetails(string t, string a, string i, bool avail, string date) {
+        title = t;
+        author = a;
+        ISBN = i;
+        availability = avail;
+        dateAdd = date;
+    }
 
-    // --- Display book details ---
-    void displayBookDetails();
+    // Display book details
+    void displayBookDetails() {
+        cout << "\nTitle       : " << title;
+        cout << "\nAuthor      : " << author;
+        cout << "\nISBN        : " << ISBN;
+        cout << "\nAvailable   : " << (availability ? "Yes" : "No");
+        cout << "\nDate Added  : " << dateAdd;
+    }
 
-    // --- Getters ---
-    string getISBN();
-    bool isAvailable();
+    // Getters
+    string getISBN() { return ISBN; }
+    bool isAvailable() { return availability; }
 
-    // --- Borrow Book ---
-    void borrowBook();
+    // Borrow book
+    void borrowBook() {
+        if (availability) {
+            availability = false;
+            cout << "Success: Book borrowed.\n";
+        }
+        else {
+            cout << "Borrowed: Book is not available.\n";
+        }
+    }
 
-    // --- Sort Books by ISBN (Bubble Sort) ---
-    static void sortBookData(Book arr[], int size);
+    // Sort books by ISBN (Bubble Sort)
+    static void sortBookData(Book arr[], int size) {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (arr[j].ISBN > arr[j + 1].ISBN) {
+                    Book temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
 };
 
 #endif
