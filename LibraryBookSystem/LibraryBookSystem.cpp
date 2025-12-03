eda7ec04121e23d707223a1cc9453e02d4c0ad23
15915 <15915@ait.nsw.edu.au>
2025-12-03 18:00:18 +1100

    modified LibraryBookSystem.cpp

--- LibraryBookSystem/LibraryBookSystem.cpp
+++ LibraryBookSystem/LibraryBookSystem.cpp
@@ -4,16 +4,32 @@ using namespace std;
 
 int main() {
 
-    // Create book list (5 books)
-    Book library[5];
+    // Use POINTER array to enable polymorphism
+    Book* library[5];
 
-    library[0].setBookDetails("Introduction of IT", "Lara Smith", "123", true, "2023-08-01");
-    library[1].setBookDetails("C++ Programming", "Wayne Kevin", "856", true, "2021-02-21");
-    library[2].setBookDetails("Algorithms", "Robert Sedgewick", "736", true, "2021-02-21");
-    library[3].setBookDetails("Networking", "Henry Lee", "013", true, "2024-06-04");
-    library[4].setBookDetails("Cyber Security", "Luis Jones", "633", true, "2024-02-05");
+    // Create objects (mix of Parent, EBook, Hardcopy)
+    library[0] = new Book();
+    library[1] = new EBook();
+    library[2] = new HardcopyBook();
+    library[3] = new EBook();
+    library[4] = new HardcopyBook();
 
-    // Sort books by ISBN
+    // Set book details
+    library[0]->setBookDetails("Introduction of IT", "Lara Smith", "123", true, "2023-08-01");
+
+    library[1]->setBookDetails("C++ Programming", "Wayne Kevin", "856", true, "2021-02-21");
+    ((EBook*)library[1])->setLicense("2025-12-31");
+
+    library[2]->setBookDetails("Algorithms", "Robert Sedgewick", "736", true, "2021-02-21");
+    ((HardcopyBook*)library[2])->setShelf("A-12");
+
+    library[3]->setBookDetails("Networking", "Henry Lee", "013", true, "2024-06-04");
+    ((EBook*)library[3])->setLicense("2026-01-01");
+
+    library[4]->setBookDetails("Cyber Security", "Luis Jones", "633", true, "2024-02-05");
+    ((HardcopyBook*)library[4])->setShelf("B-03");
+
+    // Sort all books by ISBN
     Book::sortBookData(library, 5);
 
     string searchISBN;
@@ -22,9 +38,9 @@ int main() {
         cin >> searchISBN;
 
         if (searchISBN == "0") {
-            cout << "\n---- FINAL BOOK LIST ----";
+            cout << "\n---- FINAL BOOK LIST ----\n";
             for (int i = 0; i < 5; i++) {
-                library[i].displayDetails();
+                library[i]->displayDetails();
                 cout << "\n";
             }
             break;
@@ -32,14 +48,21 @@ int main() {
 
         bool found = false;
         for (int i = 0; i < 5; i++) {
-            if (library[i].getISBN() == searchISBN) {
+            if (library[i]->getISBN() == searchISBN) {
                 found = true;
-                library[i].borrowBook();
+                library[i]->borrowBook();
                 break;
             }
         }
 
-        if (!found) cout << "Not Found: No book with this ISBN.\n";
+        if (!found) {
+            cout << "Not Found: No book with this ISBN.\n";
+        }
+    }
+
+    // Clean up memory
+    for (int i = 0; i < 5; i++) {
+        delete library[i];
     }
 
     return 0;
