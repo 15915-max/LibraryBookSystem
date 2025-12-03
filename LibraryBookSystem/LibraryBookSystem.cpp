da0aef296bc3e77e54ca661108b117a499b448b9
15915 <15915@ait.nsw.edu.au>
2025-12-03 19:21:55 +1100

    Ascending & Descending & Mixed order added

--- LibraryBookSystem/LibraryBookSystem.cpp
+++ LibraryBookSystem/LibraryBookSystem.cpp
@@ -4,6 +4,42 @@ using namespace std;
 
 int main() {
 
+
+    cout << "\n===== A: Ascending Order Input =====\n";
+    Book asc[3];
+    asc[0].setBookDetails("Introduction of IT", "Lara Smith", "123", true, "2023-08-01");
+    asc[1].setBookDetails("C++ Programming", "Wayne Kevin", "856", true, "2021-02-21");
+    asc[2].setBookDetails("Algorithms", "Robert Sedgewick", "736", true, "2021-02-21");
+
+    for (int i = 0; i < 3; i++) {
+        asc[i].displayDetails();
+        cout << "\n";
+    }
+
+    cout << "\n===== B: Descending Order Input =====\n";
+    Book desc[3];
+    desc[0].setBookDetails("Algorithms", "Robert Sedgewick", "736", true, "2021-02-21");
+    desc[1].setBookDetails("C++ Programming", "Wayne Kevin", "856", true, "2021-02-21");
+    desc[2].setBookDetails("Introduction of IT", "Lara Smith", "123", true, "2023-08-01");
+
+    for (int i = 0; i < 3; i++) {
+        desc[i].displayDetails();
+        cout << "\n";
+    }
+
+    cout << "\n===== C: Mixed Order Input =====\n";
+    Book mix[3];
+    mix[0].setBookDetails("C++ Programming", "Wayne Kevin", "856", true, "2021-02-21");
+    mix[1].setBookDetails("Algorithms", "Robert Sedgewick", "736", true, "2021-02-21");
+    mix[2].setBookDetails("Introduction of IT", "Lara Smith", "123", true, "2023-08-01");
+
+    for (int i = 0; i < 3; i++) {
+        mix[i].displayDetails();
+        cout << "\n";
+    }
+
+
+
     // Use POINTER array to enable polymorphism
     Book* library[5];
 
@@ -29,9 +65,10 @@ int main() {
     library[4]->setBookDetails("Cyber Security", "Luis Jones", "633", true, "2024-02-05");
     ((HardcopyBook*)library[4])->setShelf("B-03");
 
-    // Sort all books by ISBN
+    // Sort library
     Book::sortBookData(library, 5);
 
+    // Borrowing system
     string searchISBN;
     while (true) {
         cout << "\nEnter ISBN to borrow (0 to quit): ";
@@ -60,10 +97,8 @@ int main() {
         }
     }
 
-    // Clean up memory
-    for (int i = 0; i < 5; i++) {
-        delete library[i];
-    }
+    // Clean up
+    for (int i = 0; i < 5; i++) delete library[i];
 
     return 0;
 }
