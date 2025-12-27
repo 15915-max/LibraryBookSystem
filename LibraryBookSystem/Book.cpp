edb907cc2db901e21ab34bcd7079df279de5f449
15915-max <15915@ait.nsw.edu.au>
2025-12-27 15:41:37 +1100

    Implement ISBN validation in setBookDetails
    Added validation for ISBN in setBookDetails method.

--- LibraryBookSystem/Book.cpp
+++ LibraryBookSystem/Book.cpp
@@ -18,6 +18,26 @@ Book::Book(string t, string a, int i, bool avail, string date) {
     dateAdd = date;
 }
 
+bool Book::isValidISBN(int i) const {
+    return (i > 99);
+}
+
+bool Book::setBookDetails(string t, string a, int i, bool avail, string date) {
+
+    if (!isValidISBN(i)) {
+        cout << "Error: Invalid ISBN number" << endl;
+        return false;
+    }
+
+    title = t;
+    author = a;
+    isbn = i;
+    availability = avail;
+    dateAdd = date;
+
+    return true;
+}
+
 int Book::getISBN() const {
     return isbn;
 }
